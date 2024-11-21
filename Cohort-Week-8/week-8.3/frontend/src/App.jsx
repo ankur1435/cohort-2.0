69a9f3e83d5016a6dd2c0ed9c6983860c5ae8bde
unknown <rohitpithani13@gmail.com>
2024-11-21 21:16:41 +0530

    Week-8.3 Completed

--- Cohort-Week-8/week-8.3/frontend/src/App.jsx
+++ Cohort-Week-8/week-8.3/frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/signup' element={< Signup />} />
-          <Route path='/signin' element={< Signin />} />
+          <Route path='/' element={< Signin />} />
           <Route path='/dashboard' element={< Dashboard />} />
           <Route path='/send' element={< SendMoney />} />
         </Routes>
--- Cohort-Week-8/week-8.3/frontend/src/components/Heading.jsx
+++ Cohort-Week-8/week-8.3/frontend/src/components/Heading.jsx
@@ -1,5 +1,6 @@
+
 export function Heading({label}) {
-    return <div className="font-bold text-4xl pl-6">
+    return <div className="font-bold text-4xl pt-6">
         {label}
     </div>
 }
