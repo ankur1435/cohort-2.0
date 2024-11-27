1bec629913b2f0d20d74ab9330722f6265e63461
unknown <rohitpithani13@gmail.com>
2024-11-27 20:56:49 +0530

    Week-9.2 learned about ENUMS

---
+++ Cohort-Week-9/week-9.2/b.ts
@@ -0,0 +1,17 @@
+enum Direction {
+    up,
+    down,
+    left,
+    right
+}
+
+function doSomething(keyPressed: Direction) {
+    if(keyPressed == Direction.up) {
+
+    }
+}
+
+doSomething(Direction.up);
+doSomething(Direction.down);
+doSomething(Direction.left);
+doSomething(Direction.right);
