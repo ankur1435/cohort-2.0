0e64ded143006e7dba6ff1621a1618e6c0102d31
unknown <rohitpithani13@gmail.com>
2024-12-07 22:40:35 +0530

    Week-10.1 Started

---
+++ Cohort-Week-10/week-10.1/week-10.1.1/src/index.ts
@@ -0,0 +1,25 @@
+// write a function to create a users table in ypur database.
+import { Client } from 'pg';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+});
+
+async function createUsersTable() {
+    await client.connect();
+    const result = await client.query(`
+            CREATE TABLE users (
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(50) UNIQUE NOT NULL,
+                email VARCHAR(255) UNIQUE NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+            );
+        `)
+    console.log(result);
+}
+
+createUsersTable();
