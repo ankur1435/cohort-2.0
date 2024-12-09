3fdc3078635cc1b6cb89bae755b4daffe5f72fe5
unknown <rohitpithani13@gmail.com>
2024-12-09 19:42:53 +0530

    Week-10.1.1 Inserted and learned some basic SQL queries

--- Cohort-Week-10/week-10.1/week-10.1.1/src/insert.ts
+++ Cohort-Week-10/week-10.1/week-10.1.1/src/insert.ts
@@ -1,3 +1,25 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
-// Async function to insert
+// Async function to insert data into a table
+async function insertData() {
+    const client = new Client({
+        host: 'localhost',
+        port: 5432,
+        database: 'postgres',
+        user: 'postgres',
+        password: 'rohit@026'
+    });
+
+    try{
+        await client.connect();
+        const insertQuery = "INSERT INTO users (username, email, password) VALUES ('username', 'username@example.com', 'userpassword');";
+        const res = await client.query(insertQuery);
+        console.log("Insertion success", res); // Output insertion result
+    } catch(err) {
+        console.log("Error during the insertion", err);
+    } finally {
+        await client.end(); // Clone the client connection
+    }
+}
+
+insertData();
