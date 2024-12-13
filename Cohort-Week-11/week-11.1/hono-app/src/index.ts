654cec271aed00a0d0e1f8ac8748ea3c08e20dd9
unknown <rohitpithani13@gmail.com>
2024-12-13 20:30:20 +0530

    Week-11.1

--- Cohort-Week-11/week-11.1/hono-app/src/index.ts
+++ Cohort-Week-11/week-11.1/hono-app/src/index.ts
@@ -2,8 +2,19 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', async (c) => {
+async function authMiddleware(c: any, next: any) {
+  // context of this request, request, response
+  if(c.req.header("Authorization")) {
+    await next()
+  } else {
+    return c.text("You dont have access")
+  }
+} 
+
+// fetch => json
+app.post('/', authMiddleware, async (c) => {
   const body = await c.req.json()
+
   console.log(body);
   console.log(c.req.header("Authentication"));
   console.log(c.req.query("param"));
