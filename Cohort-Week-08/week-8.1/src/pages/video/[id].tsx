32a402e62705a9f8fcbfb490102b1b98409599da
unknown <rohitpithani13@gmail.com>
2024-12-19 20:23:52 +0530

    Week-8.1 Youtube Clone

--- Cohort-Week-08/week-8.1/src/pages/video/[id].tsx
+++ Cohort-Week-08/week-8.1/src/pages/video/[id].tsx
@@ -8,7 +8,7 @@ export default function VideoPage() {
         <Appbar />
         <div className="grid gap-4 grid-cols-12 pt-5">
             <div className="col-span-12 md:col-span-9 sm:col-span-8">
-                <video width={"100%"} height={"720px"} controls src="https://dxip4wcd8mhk4.cloudfront.net/These+Coding+Projects+Give+You+An+Unfair+Advantage.mp4" />
+                <video width={"100%"} height={"720px"} controls src="https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4" />
                 <div className="text-2xl">
                     How to get better at coding
                 </div>
