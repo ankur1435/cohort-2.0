627069bb89792a5b4ba6e477ec711350c7ae2fe2
RohitPithani026 <rohitpithani13@gmail.com>
2025-01-18 09:45:05 +0530

    Deleted files

--- Cohort-Week-19/week-19.1/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-let requestCount = 0;
-export function middleware(req: NextRequest, ) {
-    requestCount++;
-    const res = NextResponse.next()
-    console.log("Request Count: ", requestCount);
-    return res;
-}
