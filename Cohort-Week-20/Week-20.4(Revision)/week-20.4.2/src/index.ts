43391434ac38069b62978f14856e21b9d3b0d94b
RohitPithani026 <rohitpithani13@gmail.com>
2025-02-20 07:09:23 +0530

    Added Week-20.4.1 and Week-20.4.2

---
+++ Cohort-Week-20/Week-20.4(Revision)/week-20.4.2/src/index.ts
@@ -0,0 +1,68 @@
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi"
+import { ParamsSchema } from "./input"
+import { UserSchema } from "./output"
+const app = new OpenAPIHono();
+
+const getUserRoute = createRoute({
+  method: 'get',
+  path: '/user/{id}',
+  request: {
+    params: ParamsSchema
+  },
+  responses: {
+    200: {
+      content: {
+        'application/json': {
+          schema: UserSchema
+        }
+      }, 
+      description: "Get the user details"
+    }
+  }
+})
+
+const postUserRoute = createRoute({
+  method: 'post',
+  path: '/user/{id}',
+  request: {
+    params: ParamsSchema
+  },
+  responses: {
+    200: {
+      content: {
+        'application/json': {
+          schema: UserSchema
+        }
+      }, 
+      description: "Get the user details"
+    }
+  }
+})
+
+app.openapi(getUserRoute, (c) => {
+  const { id } = c.req.valid("param");
+  return c.json({
+    id, 
+    age: 20,
+    name: "Ultra-man",
+  })
+})
+
+app.openapi(postUserRoute, (c) => {
+  const { id } = c.req.valid("param");
+  return c.json({
+    id, 
+    age: 20,
+    name: "Ultra-man",
+  })
+})
+
+app.doc('/doc', {
+  openapi: '3.0.0',
+  info: {
+    version: '1.0.0',
+    title: 'My API',
+  }
+})
+
+export default app
