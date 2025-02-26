b912ed99eb90229229dda240f0c2c664dd78ed01
RohitPithani026 <rohitpithani13@gmail.com>
2025-02-26 22:59:46 +0530

    Week-20.4.4(Revision of decorators and classes)

--- Cohort-Week-20/Week-20.4(Revision)/week-20.4.4/classes/src/index.js
+++ Cohort-Week-20/Week-20.4(Revision)/week-20.4.4/classes/src/index.js
@@ -1,32 +1,26 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const helpful_decorators_1 = require("helpful-decorators");
 class DateClass {
     constructor(timeZone) {
         this.timeZone = timeZone;
     }
     getTime() {
         var d = new Date();
+        console.log("hi from getTime");
         return d.getTime();
     }
-    getMonth() {
-        var d = new Date();
-        return d.getMonth();
-    }
-    getTimeZone() {
-        return this.timeZone;
-    }
-    expensiveOperations() {
-        // here
-        const startTime = new Date().getTime(); ///
-        let ctr = 0;
-        for (let i = 0; i <= 10000000000; i++) {
-            ctr++;
-        }
-        console.log(ctr);
-        const endTime = new Date().getTime(); ///
-        console.log("Total time taken is " + (endTime - startTime) + " ms");
-        // here 
-    }
 }
+__decorate([
+    helpful_decorators_1.measure
+], DateClass.prototype, "getTime", null);
 const dateObject = new DateClass("IND");
-const response = dateObject.expensiveOperations();
-console.log(response);
+dateObject.getTime();
+dateObject.getTime();
+dateObject.getTime();
--- Cohort-Week-20/Week-20.4(Revision)/week-20.4.4/classes/src/index.ts
+++ Cohort-Week-20/Week-20.4(Revision)/week-20.4.4/classes/src/index.ts
@@ -1,37 +1,22 @@
+import { measure } from "helpful-decorators";
+
 class DateClass {
     private timeZone: string;
     constructor(timeZone: string) {
         this.timeZone = timeZone;
     } 
     
+    //@ts-ignore
+    @measure
     getTime() {
         var d = new Date();
+        console.log("hi from getTime")
         return d.getTime();
     }
 
-    getMonth() {
-        var d = new Date();
-        return d.getMonth();
-    }
-
-    getTimeZone() {
-        return this.timeZone;
-    }
-
-    expensiveOperations() {
-        // here
-        const startTime = new Date().getTime(); ///
-        let ctr = 0;
-        for (let i=0; i<=10000000000; i++) {
-            ctr++;
-        }
-        console.log(ctr);
-        const endTime = new Date().getTime(); ///
-        console.log("Total time taken is " + (endTime - startTime)  + " ms"); 
-        // here 
-    }
 }
 
 const dateObject = new DateClass("IND");
-const response = dateObject.expensiveOperations();
-console.log(response);
+dateObject.getTime();
+dateObject.getTime();
+dateObject.getTime();
