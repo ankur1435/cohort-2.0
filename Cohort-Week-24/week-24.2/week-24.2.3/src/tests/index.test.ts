8591a17aa5a8ad8b330e347c4117087b84a72b49
RohitPithani026 <rohitpithani13@gmail.com>
2025-04-07 06:59:24 +0530

    Week-24.2 Implemented supertest and vitest testing

---
+++ Cohort-Week-24/week-24.2/week-24.2.3/src/tests/index.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest';
+import request from "supertest";
+import { app } from '../index';
+
+describe("POST /sum", () => {
+    it("should return the sum of two numbers", async() => {
+        const res = await request(app).post("/sum").send({
+            a: 1,
+            b: 2
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.answer).toBe(3);
+    });
+
+    it("should return the sum of two negative numbers", async() => {
+        const res = await request(app).post("/sum").send({
+            a: -1,
+            b: -2
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.answer).toBe(-3);
+    });
+});
