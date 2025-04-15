573e54f387fc0830a1a467d7c250578720c8f594
RohitPithani026 <rohitpithani13@gmail.com>
2025-04-15 21:30:11 +0530

    Week-26.2 Implemented Prometheus

--- Cohort-Week-26/week-26.2/src/monitoring/requestCount.ts
+++ Cohort-Week-26/week-26.2/src/monitoring/requestCount.ts
@@ -1,17 +1,23 @@
 import { Request, Response, NextFunction } from "express";
 import client from "prom-client";
 
-const requestCounter = new client.Counter({
-    name: "request_count",
-    help: "Total request count",
-    labelNames: ["method", "route"]
+export const httpRequestDurationMicroseconds = new client.Histogram({
+    name: 'http_request_duration_ms',
+    help: 'Duration of HTTP requests in ms',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [0.1, 5, 15, 50, 100, 300, 500, 1000, 3000, 5000]
 });
 
 export function requestCount(req: Request, res: Response, next: NextFunction) {
+    const startTime = Date.now();
     
-    requestCounter.inc({
-        method: req.method,
-        route: req.route ? req.route.path : req.path,
+    res.on("finish", () => {
+        const endTime = Date.now();
+        httpRequestDurationMicroseconds.observe({
+            method: req.method,
+            route: req.body ? req.route.path : req.path,
+            code: res.statusCode
+        }, endTime - startTime);
     });
 
     next();
